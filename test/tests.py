9e19d14631fbb5917a55f9b5e88f2390a512d7b9
mm116 <michael_markell@brown.edu>
2017-12-28 01:08:04 -0500

    adding delayed assert

--- test/tests.py
+++ test/tests.py
@@ -2,28 +2,35 @@ import sys
 sys.path.insert(0, '..')
 from loader import load, parse
 from solver import solve
+from delayed_assert import expect, assert_expectations
 
 def solve_it(arg):
 	file = load(arg)
 	parsed = parse(file)
 	return solve(parse(file))
 
-class Tester():
+def check_pass(arg):
+	expect(solve_it('../boards/' + arg) == True)
 
-	def assert_pass(self, arg):
-		assert solve_it('../boards/' + arg) == True
+def test_one():
+	check_pass('board_one.txt')
 
-	def test_everything(self):
-		self.assert_pass('board_one.txt')
-		self.assert_pass('board_two.txt')
-		self.assert_pass('board_three.txt')
-		self.assert_pass('board_four.txt')
-		self.assert_pass('board_five.txt')
-		self.assert_pass('board_six.txt')
-		self.assert_pass('board_seven.txt')
+def test_two():
+	check_pass('board_two.txt')
 
-def main():
-	tester = Tester()
-	tester.test_everything()
+def test_three():
+	check_pass('board_three.txt')
 
-main()
+def test_four():
+	check_pass('board_four.txt')
+
+def test_five():
+	check_pass('board_five.txt')
+
+def test_six():
+	check_pass('board_six.txt')
+	
+def test_seven():
+	check_pass('board_seven.txt')
+
+assert_expectations()
